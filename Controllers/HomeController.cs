7956ca0b4ffa19dfdd66ee4aee67fe5257b3bf60
jhadley1922 <90360017+jhadley1922@users.noreply.github.com>
2022-03-29 13:46:14 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MIssion13.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIssion13.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BowlersDbContext _context { get; set; }
+
+        public HomeController(BowlersDbContext temp)
+        {
+            _context = temp;
+        }
+
+        // Home page with contact info table
+        public IActionResult Index()
+        {
+            // Get list of bowlers from the db
+            var bowlers = _context.Bowlers.ToList();
+
+            return View(bowlers);
+        }
+
+        // Display page with form to add a new bowler
+        [HttpGet]
+        public IActionResult NewBowler()
+        {
+            ViewBag.Teams = _context.Teams.ToList();
+
+            return View();
+        }
+
+        // Submit form to add the new bowler
+        [HttpPost]
+        public IActionResult NewBowler(Bowler b)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(b);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else // if invalid
+            {
+                ViewBag.Teams = _context.Teams.ToList();
+
+                return View();
+            }
+        }
+
+        // Display form with selecter bowler's info to edit
+        [HttpGet]
+        public IActionResult Edit(int bowlerid)
+        {
+            ViewBag.Teams = _context.Teams.ToList();
+
+            // Get bowler data
+            var bowler = _context.Bowlers.Single(x => x.BowlerID == bowlerid);
+
+            return View("NewBowler", bowler);
+        }
+
+        // Save edits to the db
+        [HttpPost]
+        public IActionResult Edit(Bowler b)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(b);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else // if invalid
+            {
+                ViewBag.Teams = _context.Teams.ToList();
+
+                return View("NewBowler", b);
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int bowlerid)
+        {
+            var movie = _context.Bowlers.Single(x => x.BowlerID == bowlerid);
+
+            return RedirectToAction("index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Bowler b)
+        {
+            _context.Bowlers.Remove(b);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Bowler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MIssion13.Models
+{
+    public class Bowler
+    {
+        [Key]
+        [Required]
+        public int BowlerID { get; set; }
+        [Required(ErrorMessage = "Please enter a first name.")]
+        public string BowlerLastName { get; set; }
+        [Required(ErrorMessage = "Please enter a Last name.")]
+        public string BowlerFirstName { get; set; }
+        public string BowlerMiddleInit { get; set; }
+        [Required(ErrorMessage = "Please enter an address.")]
+        public string BowlerAddress { get; set; }
+        [Required(ErrorMessage = "Please enter a city.")]
+        public string BowlerCity { get; set; }
+        [Required(ErrorMessage = "Please enter a State.")]
+        public string BowlerState { get; set; }
+        [Required(ErrorMessage = "Please enter a zip.")]
+        public string BowlerZip { get; set; }
+        [Required(ErrorMessage = "Please enter a phone number.")]
+        public string BowlerPhoneNumber { get; set; }
+
+        // FK Relationship
+        [Required(ErrorMessage = "Please select a team")]
+        public int TeamID { get; set; }
+        public Team Team { get; set; }
+    }
+}
