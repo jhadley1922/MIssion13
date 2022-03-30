def45d6c7eadec5b825555456c131494d0e3af1b
jhadley1922 <90360017+jhadley1922@users.noreply.github.com>
2022-03-30 17:27:11 -0600

    more updates

--- Startup.cs
+++ Startup.cs
@@ -54,10 +54,8 @@ namespace MIssion13
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            {               
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
